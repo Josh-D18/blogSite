7926c5be56ea9ff6e6f88832a6510c4251d52c27
Josh D <jjdate@live.com>
2023-07-14 11:27:00 -0400

    added type for container component

--- src/app/components/Container/Container.tsx
+++ src/app/components/Container/Container.tsx
@@ -1,6 +1,10 @@
 import React, { ReactNode } from "react";
 
-const Container = ({ children }: { children: ReactNode }) => {
+const Container: ({
+  children,
+}: {
+  children: ReactNode;
+}) => React.JSX.Element = ({ children }: { children: ReactNode }) => {
   return <div className="h-full w-full">{children}</div>;
 };
 
