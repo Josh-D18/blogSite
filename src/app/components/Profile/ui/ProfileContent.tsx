de748d236eabeccb37f710f83f22c3e614c0d217
Josh D <jjdate@live.com>
2023-07-15 02:02:32 -0400

    Fixed spacing issue with content paragraph

--- src/app/components/Profile/ui/ProfileContent.tsx
+++ src/app/components/Profile/ui/ProfileContent.tsx
@@ -6,14 +6,14 @@ const ProfileContent: React.FC = () => {
     <div className="h-full mt-[84px]">
       <div>
         <h2 className="text-lg font-semibold text-center mt-7">Josh Date</h2>
-        <p className="pt-4 text-center md:px-5 lg:px-10">
+        <p className="pt-4 mx-10 text-center md:max-w-lg md:m-auto lg:px-10 xl:max-w-xl lg:max-w-2xl">
           Lorem ipsum dolor sit amet consectetur adipisicing elit. Eligendi
           voluptatum nisi repellendus distinctio aliquid perspiciatis dolorem
           sed. Nisi vel quibusdam assumenda delectus minima vero! Neque
           inventore sed dignissimos at fugit!
         </p>
         <div className="flex items-center justify-center mx-3 mt-8">
-          <div className="border-4 border-[#C0C0C0] shadow-lg p-3 my-8 rounded-lg w-full md:max-w-lg">
+          <div className="border-4 border-[#C0C0C0] shadow-lg p-3 my-8 rounded-lg w-full max-w-lg ">
             <h3 className="font-semibold">Title to Blog</h3>
             <p className="block font-normal truncate">
               Lorem ipsum dolor sit, amet consectetur adipisicing elit.
